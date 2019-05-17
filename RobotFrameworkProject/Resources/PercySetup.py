677028b8a9dd82c8ee508fa3fea53167c396fa28
InternDN19.01.07 Dinh Thi Anh Thao <InternDN19.01.07@logigear.com>
2019-05-17 14:23:27 +0700

    edit PercySetup.py file

--- RobotFrameworkProject/Resources/PercySetup.py
+++ RobotFrameworkProject/Resources/PercySetup.py
@@ -11,13 +11,13 @@ class PercySetup():
 
     def Percy_Initialize_Build(self, access_token):
         driver = BuiltIn().get_library_instance('Selenium2Library')._current_browser()
-        root_static_dir = os.path.join(os.path.dirname(__file__), 'static')
+        root_static_dir = os.path.join(os.path.dirname(__file__), '../', 'htdocs/joomla')
         loader = percy.ResourceLoader(
           root_dir=root_static_dir,
-          base_url='joomla',
+#           base_url='/assets',
           webdriver=driver,
         )
-        config = percy.Config(access_token=access_token, default_widths=[1280, 375])
+        config = percy.Config(access_token=access_token)
         self.percy_runner = percy.Runner(loader=loader, config=config)
         self.percy_runner.initialize_build()
 
